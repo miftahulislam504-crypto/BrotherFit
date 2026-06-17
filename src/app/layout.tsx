6756ca4dc9234df20c97e46f14f64f5f170985cb
miftahulislam504-crypto <miftahulislam504@gmail.com>
2026-06-17 07:13:14 +0600

    update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -99,7 +99,7 @@ export const metadata: Metadata = {
     },
   },
   verification: {
-    // google:google-site-verification=0iEtVq02RRdC_lUcDWnrKGauZah9KGHLY_e0rM0BxA8 'YOUR_GOOGLE_SEARCH_CONSOLE_VERIFICATION_CODE', // ← পরে add করবে
+    // google:google8bec4b5f7404e086 'YOUR_GOOGLE_SEARCH_CONSOLE_VERIFICATION_CODE', // ← পরে add করবে
   },
 };
 
